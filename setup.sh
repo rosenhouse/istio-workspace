3333af9543ae1a80807886cb6e7f3ccd0c7a8de7
Gabe Rosenhouse <grosenhouse@pivotal.io>
2018-02-03 20:29:07 -0800

    use workspace as gopath

--- setup.sh
+++ setup.sh
@@ -5,6 +5,13 @@ set -e -u -x -o pipefail
 export GOPATH=/go
 export PATH=/usr/local/go/bin:$GOPATH/bin:$PATH
 
-cd $GOPATH/src/istio.io/istio
-
 go get -u github.com/golang/dep/cmd/dep
+
+mkdir -p $GOPATH/src/istio.io
+cd $GOPATH/src/istio.io
+
+if ! [ -d istio ]; then
+  git clone https://github.com/istio/istio
+fi
+cd istio
+dep ensure
