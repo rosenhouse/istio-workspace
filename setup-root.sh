4a967ba1e20f021be07427ae34cf64a07ce28720
Gabe Rosenhouse <grosenhouse@pivotal.io>
2018-02-18 14:01:04 -0800

    Use Go 1.10

--- setup-root.sh
+++ setup-root.sh
@@ -5,7 +5,7 @@ apt-get update -y || (sleep 40 && apt-get update -y)
 apt-get install -y git build-essential
 
 if [ ! -d /usr/local/go ]; then
-  wget -qO- https://storage.googleapis.com/golang/go1.9.4.linux-amd64.tar.gz | tar -C /usr/local -xz
+  wget -qO- https://storage.googleapis.com/golang/go1.10.linux-amd64.tar.gz | tar -C /usr/local -xz
 fi
 
 for d in /go /go/src /go/bin /go/pkg
