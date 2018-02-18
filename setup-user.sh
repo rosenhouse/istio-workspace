b49cd62b8b076256d3b379fcf5337cab4cd495ce
Gabe Rosenhouse <grosenhouse@pivotal.io>
2018-02-18 15:33:43 -0800

    use go get for istio

--- setup-user.sh
+++ setup-user.sh
@@ -12,8 +12,4 @@ source $HOME/.profile
 
 dep version || go get github.com/golang/dep/cmd/dep
 
-mkdir -p $GOPATH/src/istio.io
-cd $GOPATH/src/istio.io
-if [ ! -d istio ]; then
-  git clone https://github.com/istio/istio
-fi
+go get -u istio.io/istio
